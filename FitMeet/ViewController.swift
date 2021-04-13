32792d9c8f1d86cc098154808bc9483038929710
Sergey Koryukin <koryukin@novotorica.com>
2021-04-13 14:03:42 +0300

    delete main.storyboard

---
+++ FitMeet/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FitMeet
+//
+//  Created by novotorica on 13.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
